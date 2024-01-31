ea014269d408bae5ecc35b2bcfe4ffcfb8dd82a6
Rob Fentress <learn@vt.edu>
2024-01-31 13:00:22 -0500

    add tests and such

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isActive: false
+  };
+
+  handleShow = () => {
+    this.setState({isActive: true});
+  };
+
+  handleHide = () => {
+    this.setState({isActive: false});
+  };
+
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          
+             <button onClick={this.handleShow}>Show</button>
+             <button onClick={this.handleHide}>Hide</button>
+          {this.state.isActive ? <h1 data-test-id="monkey">Hello React</h1> : null }
+          </header>
+        </div>
+      )
+  }
 }
 
 export default App;
